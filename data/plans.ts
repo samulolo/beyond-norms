bb2617de7639f0fd440fa5c7eb1737929e6ef982
samulolo <eliseufranco26@hotmail.com>
2026-08-05 13:59:26 +0100

    Last test done

--- data/plans.ts
+++ data/plans.ts
@@ -15,7 +15,7 @@ export const eventsPlans : PlansTypes[]  = [
         id: "1",
         name: "Full Evening Access",
         description: 'A complete experience combining Soul Speed Dating and our signature Dinner Show.',
-        price: "0.5",
+        price: "85",
         features: [
             "Welcome Drinks",
             "Soul Speed Dating Session",
